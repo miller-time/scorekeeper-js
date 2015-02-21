6bf274a3cbb1e492490b524b69f995286223baed
Russell <miller.time.baby@gmail.com>
2015-02-21 00:17:05 -0800

    add angular-ui-router

--- save_game_scores/static/app/scripts/app.js
+++ save_game_scores/static/app/scripts/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-angular.module('scorekeeper', ['ui.bootstrap']);
+angular.module('scorekeeper', ['ui.bootstrap', 'ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('home', {
+            url: '/',
+            templateUrl: '/static/app/views/home.html'
+        });
+
+    });
