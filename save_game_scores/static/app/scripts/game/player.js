ad60d9ea905687d2eb7744bac95ea5c9539a6257
Russell <miller.time.baby@gmail.com>
2015-02-21 11:17:13 -0800

    add scores to players

--- save_game_scores/static/app/scripts/game/player.js
+++ save_game_scores/static/app/scripts/game/player.js
@@ -1,5 +1,32 @@
 'use strict';
 
-window.Player = function(name) {
+window.Player = function(name, score) {
+    if (angular.isUndefined(score)) {
+        score = 0;
+    }
+
     this.name = name;
+    this.score = score;
+    this.scoreHistory = [{
+        score: score
+    }];
+    this.metadata = {};
+};
+
+window.Player.prototype.addPoints = function(delta) {
+    this.score += delta;
+    this.scoreHistory.push({
+        delta: delta,
+        score: this.score
+    });
+};
+
+/*
+    Custom Attributes
+    -----------------
+
+    Players can have additional attributes like "phase" or "level"
+*/
+window.Player.prototype.setCustomAttribute = function(attribute, value) {
+    this.metadata.attribute = value;
 };
